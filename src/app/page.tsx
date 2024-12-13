e9a11b0e752c03215f79bb25c9a0c59831c96d1b
ram verma <ramvrm5@gmail.com>
2024-12-13 11:28:33 +0530

    logout and cookie delteionfunctionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
   return (
     <div className="">
-      <Button size="xs" variant="primary">
-        primary
-      </Button>
-      <Button variant="secondary">secondary</Button>
-      <Button variant="destructive">destructive</Button>
-      <Button variant="ghost">ghost</Button>
-      <Button variant="muted">muted</Button>
-      <Button variant="outline">outline</Button>
-      <Button variant="teritary">teritary</Button>
+      Only visible to authorized users
+      <Button onClick={() => mutate()}>Logout</Button>
     </div>
   );
 }
